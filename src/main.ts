1df51c9081bf8bf543472d10ba98f3f386c14611
jjuidev <jjuidev@gmail.com>
2025-12-23 18:32:15 +0700

    feat: ✨ add bridge feature

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,4 @@
-import { createBridgeFactory, Filter } from './bridge'
-import { HttpClient } from './HttpClient'
-
 export * from './HttpClient'
 export * from './TokenManager'
 export * from './EventEmitter'
 export * from './bridge'
-
-//
-const API_BASE_URL = 'https://api.jjuidev.com'
-
-export const httpClient = new HttpClient({
-	baseURL: API_BASE_URL,
-	headers: {
-		'Content-Type': 'application/json'
-	},
-	ignoreTokenPatterns: [/auth\/login/],
-	tokenManagerOptions: {
-		expiryThreshold: 10_000,
-		executeRefreshToken: async ({ accessToken, refreshToken }) => {
-			const response = await fetch(`${API_BASE_URL}/auth/refresh-token`, {
-				method: 'POST',
-				body: JSON.stringify({ refreshToken }),
-				headers: {
-					'Content-Type': 'application/json',
-					Authorization: `Bearer ${accessToken}`
-				}
-			})
-
-			const data = await response.json()
-
-			return data.data
-		}
-	}
-})
-
-httpClient.useResponseInterceptor({
-	onResponse: async (response) => response.data,
-	onResponseError: async (error) => Promise.reject(error)
-})
-
-export const { createQueryKey, createService } = createBridgeFactory({ httpClient })
-
-export const authQueryKey = createQueryKey('auth', ({ buildQueryKey }) => ({
-	list23: (filter?: Filter) => buildQueryKey(filter)
-}))
